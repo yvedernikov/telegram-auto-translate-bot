0d08b9f2979c41f231adf3c2090d09130cd37fe7
yvedernikov <vedernikov.yegor@gmail.com>
2025-04-03 17:01:46 +0300

    Remove secret

--- config.py
+++ config.py
@@ -1,5 +1,5 @@
 # Telegram Bot Token (from @BotFather)
-BOT_TOKEN = "7215403613:AAEPPL63UGpvo9ljHdSgEACvuf_9fY0Dwdo"
+BOT_TOKEN = "YOUR_TOKEN"
 
 # Translation Settings
 TARGET_LANG = "uk"  # Change to your target language (e.g., "fr", "de")
